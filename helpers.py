b3d9903ebc3babd35641efe9930696f7371f5122
lpknv <nikitalipkanov@outlook.com>
2026-03-30 10:47:58 +0200

    update currency helper

--- helpers.py
+++ helpers.py
@@ -1,8 +1,11 @@
 import locale
 
-locale.setlocale(locale.LC_ALL, 'en_US')
+try:
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+except locale.Error:
+    locale.setlocale(locale.LC_ALL, '')  # fallback auf System
 
 
 def format_currency(amount):
     """format currency as dollars"""
-    return '${:,.2f}'.format(amount)
+    return locale.currency(amount, grouping=True)
